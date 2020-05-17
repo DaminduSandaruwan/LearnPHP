d4a271f722746046c6caae2cf516fed410621bc6
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-05-17 21:32:39 +0530

    Output In Browser

---
+++ outputs.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "Hello, ";
+        print "Damindu"; //string 
+        print 12; //interger
+        echo 10 + 5; //can do calculations
+    ?>  
+</body>
+</html>
