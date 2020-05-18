6dc164b3ec017cabada8c536e8ebabf9f5d91b1c
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-05-18 22:35:48 +0530

    Increment and Decrement Operators

---
+++ IncrementAndDecrement.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $x = 10;
+        echo $x;
+        echo "<br>";
+        echo ++$x; //add one
+
+        echo "<br>";
+
+        $x = 10;
+        echo $x;
+        echo "<br>";
+        echo $x++;  
+        echo "<br>";
+        echo $x;
+
+        echo "<br>";
+
+        
+        $x = 10;
+        echo $x;
+        echo "<br>";
+        echo --$x; 
+
+        echo "<br>";
+
+        $x = 10;
+        echo $x;
+        echo "<br>";
+        echo $x--;  
+        echo "<br>";
+        echo $x;
+
+    ?>
+</body>
+</html>
