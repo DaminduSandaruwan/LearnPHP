fde60e2e72a19ea8286dc952b2fa956aba0746e7
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-05-18 21:30:00 +0530

    Arithamatic Operation

---
+++ ArithamaticOperators.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        echo 5;
+        echo "<br>";
+        echo 5+5;
+        echo "<br>";
+        echo 15-5;
+        echo "<br>";
+        echo 15/5;
+        echo "<br>";
+        echo 5*5;
+        echo "<br>";
+        echo 27%5; //Modulus
+        echo "<br>";
+        echo 5**2; //Power
+    ?>
+</body>
+</html> 
