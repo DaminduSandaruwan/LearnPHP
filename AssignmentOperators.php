fd2fe410b455ca1a1f03b8484c6111de6c646aaa
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-05-18 21:55:46 +0530

    Assignment Operators

---
+++ AssignmentOperators.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        //Assignment Operators
+        $x = 100;
+        echo $x;
+        echo "<br>";
+
+        $x = $x + 20;
+        echo $x;
+        echo "<br>";
+
+        $x += 20;
+        echo $x;
+        echo "<br>";
+
+        $x /=5;
+        echo $x;
+
+    ?>
+</body>
+</html>
