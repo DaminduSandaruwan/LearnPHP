93800a4ae3041b30ce20a8912dace6b7f9a0e0e5
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-05-19 22:38:09 +0530

    Conditional Statements

---
+++ ConditioalStatement.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $x = 12 ;
+
+        if($x == 1){
+            echo "DS is very Handsome!";
+        }
+        elseif($x == 12){
+            echo "Number is 12";
+        }
+        elseif($x == 15){
+            echo "Number is 15";
+        }
+        else{
+            echo "DS is very ugly!";
+        }
+    ?>
+</body>
+</html>
