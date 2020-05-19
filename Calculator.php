8693dd3e415afa690aaba1d11058da74e03a0ebe
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-05-19 23:49:52 +0530

    Create a form for make Cal

---
+++ Calculator.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculator</title>
+</head>
+<body>
+
+     <form action="">
+        <input type="text" name="num1" placeholder="Number 1">
+        <input type="text" name="num2" placeholder="Number 2">
+        <select name="operator">
+            <option>None</option>
+            <option>Add</option>
+            <option>Substract</option>
+            <option>Multiply</option>
+            <option>Divide</option>
+        </select>
+        <br><br>
+        <button type="submit" name="submit" value="submit">Calculate</button>
+     </form>
+
+    <?php 
+     
+    ?>
+</body>
+</html>
