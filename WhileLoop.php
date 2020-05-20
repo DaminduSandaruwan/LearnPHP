81beddcc35f8871272615e9e56c2d0cfcb2e306a
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-05-20 21:47:33 +0530

    While Loop

---
+++ WhileLoop.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $x = 1;
+        while($x < 5){
+            echo "Hi...!!!<br>";
+            $x++;
+        }
+    ?>
+</body>
+</html>
