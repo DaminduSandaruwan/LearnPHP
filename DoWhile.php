dd6dd11168ede4908e426132c125ba5c3b6369d2
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-05-20 21:57:00 +0530

    Do While Loop

---
+++ DoWhile.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $x = 10;
+        do{
+            echo "Hi...!!!<br>";
+            $x++;
+        }
+        while($x < 5);
+    ?>
+</body>
+</html>
