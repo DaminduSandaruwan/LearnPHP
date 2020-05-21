83ff9386c35a5d9b1361d1a8cad449b6c3fcf2d2
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-05-21 19:04:30 +0530

    Foreach Loop

---
+++ ForEach.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $array = array("Damindu","Sandaruwan","Bandara");
+
+        foreach ($array as $loopdata){
+            echo "My Name is " . $loopdata . "<br>";
+        }
+        
+    ?>
+</body>
+</html>
