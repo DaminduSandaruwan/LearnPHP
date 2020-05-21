b8d9bc88de685b5181d3843d81d137a0838f2e2d
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-05-21 19:30:49 +0530

    Arrays

---
+++ Array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $array = arrayZ("Damindu","Sandaruwan","Bandara","Kalugalagedara");
+
+        echo $array[0];
+        
+
+    ?>
+</body>
+</html>
