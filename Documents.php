4127a61e9ad97be849dfce99d70b0265f3b3358d
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-05-22 21:24:54 +0530

    Include external Documents

---
+++ Documents.php
@@ -0,0 +1,20 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        include 'header.php';
+    ?>
+    <section> 
+        <div class="someMainContent">
+            <h1>Hi there!</h1>
+        </div>
+    </section>  
+</body>
+</html> 
---
+++ Header.php
@@ -0,0 +1,10 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="HomeDocument.php">nav1</a></li>
+            <li><a href="">nav2</a></li>
+            <li><a href="">nav3</a></li>
+            <li><a href="">nav4</a></li>
+        </ul>
+    </nav>
+</header> 
