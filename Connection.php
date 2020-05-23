26d9b4a5669cc3e21191bea9e0bfb8521b563efa
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-05-23 23:51:31 +0530

    Creating connection to DB

---
+++ Connection.php
@@ -0,0 +1,9 @@
+<?php
+
+    $dbServername = "localhost";
+    $dbusername ="root";
+    $dbpassword="";
+    $dbname="damindu";
+
+    $conn = mysqli_connect($dbServername,$dbusername,$dbpassword,$dbname);
+
