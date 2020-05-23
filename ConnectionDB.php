dcdbbf936c31b9f2baffafccfe3020239518de32
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-05-23 23:57:13 +0530

    Getting Data from DB

--- ConnectionDB.php
+++ ConnectionDB.php
@@ -7,6 +7,16 @@
     <title>Document</title>
 </head>
 <body>
-    $conn;
+    <?php 
+        $sql = "SELECT * FROM student;";
+        $result = mysqli_query($conn,$sql);
+        $resultCheck = mysqli_num_rows($result);
+
+        if($resultCheck > 0){
+            while($row = mysqli_fetch_assoc($result)){
+                echo $row['Name'] ."<br>";
+            }
+        }
+    ?>
 </body>
 </html>
