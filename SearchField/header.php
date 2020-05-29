f3c11f76d5e6bf86b4c6c29e2b238922897420d6
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-05-29 20:14:34 +0530

    Add style

--- SearchField/header.php
+++ SearchField/header.php
@@ -9,5 +9,7 @@
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <title>Document</title>
      <link rel="stylesheet" type="text/css" href="style.css">
- </head>
- <body>
+
+</head>
+
+<body>
--- SearchField/index.php
+++ SearchField/index.php
@@ -18,13 +18,12 @@
                 while($row = mysqli_fetch_assoc($result)){
                     echo "
 
-                        <div>
+                        <div class='article-box'>
                             <h3>".$row['a_title']."</h3>
                             <p>".$row['a_text']."</p> 
                             <p>".$row['a_date']."</p>
                             <p>".$row['a_author']."</p> 
                         </div>                        
-                        
                         ";
                 }
             }
